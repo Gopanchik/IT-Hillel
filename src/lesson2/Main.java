9970faa7aaf5f3857548108e194d6b8d99ddfc07
sanyo <egp.2277@gmail.com>
2023-10-11 18:43:15 +0300

    Firsst commit

---
+++ src/lesson2/Main.java
@@ -0,0 +1,58 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] nums = new int[5];
+//        nums[0] = 45;
+//        nums[1] = 4;
+//        nums[2] = 5;
+//        nums[3] = 9;
+//        nums[4] = 23;
+//        int res = nums[0] + nums[2];
+//
+//        System.out.println(res);
+//
+//        float[] nums2 = new float[] {5.90f, 6.89f, 42.678f};
+//        System.out.println(nums2[0] + nums2[2]);
+
+//        int[] arr = new int[4];
+//        Scanner scan = new Scanner(System.in);
+//        for (int i = 0; i < arr.length; i++){
+//            System.out.println("Enter the num: ");
+//            int value = scan.nextInt();
+//            arr[i] = value;
+//            //            System.out.println("First num: " + arr[i]);
+//
+//        }
+//        int min = arr[0];
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] < min)
+//                min = arr[i];
+//        }
+//        System.out.println("Minimal: " + min);
+
+//        char[][] syms = new char[2][2];
+//        syms[0][0] = "t";
+//        System.out.println(syms[0][0]);
+
+        byte[][] nums = new byte[][] {
+                {5,7},
+                {6,5},
+                {7,9},
+        };
+        System.out.println(nums[1][0]);
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
